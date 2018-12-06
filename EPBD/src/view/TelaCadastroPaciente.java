809fc7bf43403b55a7a469c8d195f8b5811b681a
Vitor Quintino <vitor_quintino@usp.br>
2018-12-06 21:59:41 -0200

    bora

--- EPBD/src/view/TelaCadastroPaciente.java
+++ EPBD/src/view/TelaCadastroPaciente.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,13 +14,13 @@ public class TelaCadastroPaciente extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JButton btnCadastrar;
-	private JTextField txtNome, txtIdade, txtCpf, txtTelefone, txtEndereço;
-	private JLabel lblNome, lblIdade, lblCpf, lblTelefone, lblEndereço;
+	private JTextField txtNome, txtIdade, txtCpf, txtTelefone, txtEndereco;
+	private JLabel lblNome, lblIdade, lblCpf, lblTelefone, lblEndereco;
 	private JPanel pnlPrincipal;
-	private TelaProcuraCadastro t;
+	private final TelaProcuraCadastro t;
 	
 	public TelaCadastroPaciente(TelaProcuraCadastro t) {
-		this.setSize(new Dimension(600, 500));
+		this.setSize(new Dimension(600, 260));
 		this.setResizable(false);
 		this.setLayout(null);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -36,7 +37,7 @@ public class TelaCadastroPaciente extends JFrame {
 		
 		pnlPrincipal = new JPanel();
 		pnlPrincipal.setLocation(10, 10);
-		pnlPrincipal.setSize(680, 480);
+		pnlPrincipal.setSize(680, 240);
 		pnlPrincipal.setLayout(null);
 		
 		lblNome = new JLabel("Nome");
@@ -46,16 +47,16 @@ public class TelaCadastroPaciente extends JFrame {
 		
 		txtNome = new JTextField();
 		txtNome.setLocation(100, 10);
-		txtNome.setSize(200, 40);
+		txtNome.setSize(250, 40);
 		txtNome.setFont(fonte2);
 		
 		lblIdade = new JLabel("Idade");
-		lblIdade.setLocation(370, 10);
+		lblIdade.setLocation(390, 10);
 		lblIdade.setFont(fonte);
 		lblIdade.setSize(100, 40);
 		
 		txtIdade = new JTextField();
-		txtIdade.setLocation(440, 10);
+		txtIdade.setLocation(460, 10);
 		txtIdade.setSize(100, 40);
 		txtIdade.setFont(fonte2);
 		
@@ -66,8 +67,34 @@ public class TelaCadastroPaciente extends JFrame {
 		
 		txtCpf = new JTextField();
 		txtCpf.setLocation(80, 60);
-		txtCpf.setSize(200, 40);
+		txtCpf.setSize(160, 40);
 		txtCpf.setFont(fonte2);
+		
+		lblTelefone = new JLabel("Telefone");
+		lblTelefone.setLocation(260, 60);
+		lblTelefone.setFont(fonte);
+		lblTelefone.setSize(120, 40);
+		
+		txtTelefone = new JTextField();
+		txtTelefone.setLocation(380, 60);
+		txtTelefone.setSize(180, 40);
+		txtTelefone.setFont(fonte2);
+		
+		lblEndereco = new JLabel("Endereço");
+		lblEndereco.setLocation(10, 110);
+		lblEndereco.setFont(fonte);
+		lblEndereco.setSize(120, 40);
+		
+		txtEndereco = new JTextField();
+		txtEndereco.setLocation(140, 110);
+		txtEndereco.setSize(420, 40);
+		txtEndereco.setFont(fonte2);
+		
+		btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setLocation(400, 160);
+		btnCadastrar.setSize(160, 40);
+		btnCadastrar.setBorder(BorderFactory.createEmptyBorder());
+		btnCadastrar.setFont(fonte);
 		
 		pnlPrincipal.add(lblNome);
 		pnlPrincipal.add(txtNome);
@@ -75,6 +102,11 @@ public class TelaCadastroPaciente extends JFrame {
 		pnlPrincipal.add(txtIdade);
 		pnlPrincipal.add(lblCpf);
 		pnlPrincipal.add(txtCpf);
+		pnlPrincipal.add(lblTelefone);
+		pnlPrincipal.add(txtTelefone);
+		pnlPrincipal.add(lblEndereco);
+		pnlPrincipal.add(txtEndereco);
+		pnlPrincipal.add(btnCadastrar);
 		
 		this.add(pnlPrincipal);
 	}
